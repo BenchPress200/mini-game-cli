aee8e66d35ed19dac2a7e7acbc36221c1ee27c54
BenchPress200 <jms0538@naver.com>
2024-05-23 16:59:42 +0900

    feat: add method to output continue message in horse racing

--- src/main/java/com/minigame/demo/controller/game/HorseRacingController.java
+++ src/main/java/com/minigame/demo/controller/game/HorseRacingController.java
@@ -2,11 +2,14 @@ package com.minigame.demo.controller.game;
 
 import com.minigame.demo.controller.GameController;
 import com.minigame.demo.service.GameService;
+import com.minigame.demo.utils.SimpleInputUtils;
 import com.minigame.demo.utils.SimpleOutputUtils;
 import com.minigame.demo.view.input.game.GameInputManager;
 import com.minigame.demo.view.output.game.GameOutputManager;
 
 import java.io.IOException;
+
+import static com.minigame.demo.constant.MeaningfulNumber.ONE_SECOND;
 
 public class HorseRacingController implements GameController {
     private final GameInputManager gameInputManager;
@@ -27,9 +30,29 @@ public class HorseRacingController implements GameController {
     public void start() throws IOException, InterruptedException {
         printWelcomeView();
         SimpleOutputUtils.printCurrentCoin();
+
+        boolean answer = readContinue();
+
+        if (!answer) {
+            return;
+        }
+
+        SimpleOutputUtils.printDecreaseCoin();
     }
 
     private void printWelcomeView() {
         gameOutputManager.printWelcomeView();
+    }
+
+    private boolean readContinue() throws IOException, InterruptedException {
+        try {
+            return SimpleInputUtils.readContinue();
+
+        } catch (IllegalArgumentException e) {
+            SimpleOutputUtils.printYesOrNo();
+            Thread.sleep(ONE_SECOND);
+
+            return readContinue();
+        }
     }
 }
